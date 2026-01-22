408b62b634a062626cc7460ced292d84239c2915
lswchina <lisuwan@smail.nju.edu.cn>
2026-01-22 19:01:31 +0800

    support 18

---
+++ shell_scripts/18_DriveLikeAHuman.sh
@@ -0,0 +1,14 @@
+OUTPUTDIR=$1
+ROUND=$2
+TIME=$3"m"
+cd ../agents/18-DriveLikeAHuman
+source activate gptdriver
+echo $OUTPUTDIR
+echo $ROUND
+echo $TIME
+if [ ! -f "$OUTPUTDIR" ]; then
+	mkdir -p $OUTPUTDIR
+fi
+
+echo timeout $TIME python HELLM.py -output \"$OUTPUTDIR\" -round $ROUND
+timeout $TIME python HELLM.py -output "$OUTPUTDIR" -round $ROUND
