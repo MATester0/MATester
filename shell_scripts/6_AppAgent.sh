986b05282e7034c22bf69b6cae1693f40bb87989
lswchina <lisuwan@smail.nju.edu.cn>
2026-01-22 16:11:34 +0800

    refc: remove M-agents to agents

--- shell_scripts/6_AppAgent.sh
+++ shell_scripts/6_AppAgent.sh
@@ -4,7 +4,7 @@ ROUND=$3
 TIME=$4"m"
 APKID=$5
 APKNAME=$APKID".apk"
-cd ../6-AppAgent
+cd ../agents/6-AppAgent
 source activate pytorch
 echo $OUTPUTDIR
 echo $TASK
@@ -15,7 +15,7 @@ if [ ! -f "$OUTPUTDIR" ]; then
 	mkdir -p $OUTPUTDIR
 fi
 
-adb install ../dataset/apks/$APKNAME
+adb install ../../dataset/apks/$APKNAME
 # adb shell monkey -p $APKID -c android.intent.category.LAUNCHER 1
 echo timeout $TIME python learn.py --app $APKID -output \"$OUTPUTDIR\" -task \"$TASK\" -round $ROUND
 timeout $TIME python learn.py --app $APKID -output "$OUTPUTDIR" -task "$TASK" -round $ROUND
