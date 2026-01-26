c6b5e5a4ee9a31cab28133561af4f10fcad361bc
lswchina <1163418363@qq.com>
2026-01-26 10:25:33 +0800

    add script 13

---
+++ shell_scripts/13_MC-Planner.sh
@@ -0,0 +1,15 @@
+OUTPUTDIR=$1
+TASK=$2
+ROUND=$3
+TIME=$4"m"
+cd ../agents/13-MC-Planner
+source activate planner
+echo $OUTPUTDIR
+echo $ROUND
+echo $TIME
+if [ ! -f "$OUTPUTDIR" ]; then
+	mkdir -p $OUTPUTDIR
+fi
+
+echo timeout $TIME python main.py model.load_ckpt_path=../../../checkpoints/controller.pt eval.output=\"$OUTPUTDIR\" eval.task_name=\"$TASK\" eval.round=$ROUND
+timeout $TIME python main.py model.load_ckpt_path=../../../checkpoints/controller.pt eval.output="$OUTPUTDIR" eval.task_name="$TASK" eval.round=$ROUND
